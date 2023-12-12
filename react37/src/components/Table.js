aadb0b02e62194dee0619debb9722ed5f06357b8
DarkKittyInTheSack <vinhnguyen1906dev@gmail.com>
2023-12-12 20:14:22 +0700

    Update

--- react37/src/components/Table.js
+++ react37/src/components/Table.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { removeUser } from '../Redux/slice/ReducerSlice'
+import { removeVietnameseCase } from '../util/ValidationSearch'
 
 const Table = ({setValues}) => {
   const {arrUser} = useSelector((state) => state.ReducerSlice)
+  const [data,setData] = useState([])
   const dispatch = useDispatch()
+
+  const searchProgress = (keySearch) =>{
+    let searchData = arrUser.filter((item) =>{
+      return item.hoTen.toLowerCase().includes(keySearch) 
+    })
+    setData(searchData)
+  }
+
+  useEffect(() =>{
+    setData(arrUser)
+  },[arrUser])
+
   return (
     <div className="mt-5">
       <h1 className='p-4 mb-5 mx-auto w-full text-lg bg-gray-800 text-white'>Danh sách sinh viên</h1>
+      <div className='flex my-2'>
+      <input
+                type="text"
+                name="hoTen"
+                id="hoTen"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                placeholder="Vui lòng nhập thông tin tìm kiếm"
+                onChange={(event) =>{
+                  searchProgress(removeVietnameseCase(event.target.value.toLowerCase()))
+                }}
+              />
+      </div>
     <div className="relative overflow-x-auto">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -30,19 +56,8 @@ const Table = ({setValues}) => {
           </tr>
         </thead>
         <tbody>
-          {/* <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Apple MacBook Pro 17"
-            </th>
-            <td className="px-6 py-4">Silver</td>
-            <td className="px-6 py-4">Laptop</td>
-            <td className="px-6 py-4">$2999</td>
-          </tr> */}
           {
-            arrUser.map((item,index) =>{
+            data.map((item,index) =>{
               const {maNguoiDung,hoTen,email,soDienThoai} = item
               return <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
